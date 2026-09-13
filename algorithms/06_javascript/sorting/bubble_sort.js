e1b265a0c24e25f4419a8f6f241b9878ef6d1d2d
igor-kan <igorkan010@gmail.com>
2026-09-13 19:49:22 -0400

    feat(js/sorting): add bubble sort

---
+++ algorithms/06_javascript/sorting/bubble_sort.js
@@ -0,0 +1,8 @@
+function bubbleSort(arr) {
+  const a = [...arr];
+  for (let i = 0; i < a.length; i++)
+    for (let j = 0; j < a.length-i-1; j++)
+      if (a[j] > a[j+1]) [a[j],a[j+1]] = [a[j+1],a[j]];
+  return a;
+}
+module.exports = { bubbleSort };
