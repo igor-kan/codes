59fc23d4dbb788280d8229183ef80bb2e33f5d07
igor-kan <igorkan010@gmail.com>
2026-09-13 19:45:49 -0400

    feat(python/graphs): add breadth-first search

---
+++ algorithms/01_python/graphs/bfs.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    order = []
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            order.append(node)
+            queue.extend(graph.get(node, []))
+    return order
