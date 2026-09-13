00d64c1c3d7c6b73c650aee6e6c9282b69fcc5fe
igor-kan <igorkan010@gmail.com>
2026-09-13 19:45:49 -0400

    feat(python/graphs): add depth-first search (iterative)

---
+++ algorithms/01_python/graphs/dfs.py
@@ -0,0 +1,11 @@
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+    order = []
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            order.append(node)
+            stack.extend(reversed(graph.get(node, [])))
+    return order
