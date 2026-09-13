72a85fd13ca6d10de8da48a9b7c3925f287318e5
igor-kan <igorkan010@gmail.com>
2026-09-13 19:50:27 -0400

    feat(ts/dp): add LCS

---
+++ algorithms/07_typescript/dp/lcs.ts
@@ -0,0 +1,7 @@
+export function lcs(a: string, b: string): number {
+  const [m,n] = [a.length,b.length];
+  const dp = Array.from({length:m+1}, ()=>new Array(n+1).fill(0));
+  for (let i=1;i<=m;i++) for (let j=1;j<=n;j++)
+    dp[i][j] = a[i-1]===b[j-1] ? dp[i-1][j-1]+1 : Math.max(dp[i-1][j],dp[i][j-1]);
+  return dp[m][n];
+}
