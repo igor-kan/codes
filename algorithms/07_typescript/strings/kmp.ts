18602596eaf5a3e817adb3074971e5914358ce85
igor-kan <igorkan010@gmail.com>
2026-09-13 19:50:27 -0400

    feat(ts/strings): add KMP

---
+++ algorithms/07_typescript/strings/kmp.ts
@@ -0,0 +1,14 @@
+export function kmp(text: string, pat: string): number[] {
+  const lps = makeLPS(pat), matches: number[] = []; let j=0;
+  for (let i=0;i<text.length;) {
+    if (text[i]===pat[j]){i++;j++;}
+    if (j===pat.length){matches.push(i-j);j=lps[j-1];}
+    else if (i<text.length&&text[i]!==pat[j]) j = j?lps[j-1]:(i++,0);
+  }
+  return matches;
+}
+function makeLPS(p: string): number[] {
+  const lps=new Array(p.length).fill(0); let k=0;
+  for (let i=1;i<p.length;){if(p[i]===p[k]){lps[i++]=++k;}else if(k){k=lps[k-1];}else{i++;}}
+  return lps;
+}
