3c7e2831bb837130473b4bfaace80bb4c87a75fa
igor-kan <igorkan010@gmail.com>
2026-09-13 19:45:59 -0400

    feat(python/data_structures): add Trie (prefix tree)

---
+++ algorithms/01_python/data_structures/trie.py
@@ -0,0 +1,28 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            node = node.children.setdefault(ch, TrieNode())
+        node.is_end = True
+
+    def search(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children: return False
+            node = node.children[ch]
+        return node.is_end
+
+    def starts_with(self, prefix):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children: return False
+            node = node.children[ch]
+        return True
