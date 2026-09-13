96102a667a37b140dafb009e30f81c0ba30dcd18
igor-kan <igorkan010@gmail.com>
2026-09-13 19:46:00 -0400

    feat(python/data_structures): add Fenwick tree (BIT) for prefix sums

---
+++ algorithms/01_python/data_structures/fenwick_tree.py
@@ -0,0 +1,19 @@
+class BIT:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * (n + 1)
+
+    def update(self, i, delta):
+        while i <= self.n:
+            self.tree[i] += delta
+            i += i & (-i)
+
+    def query(self, i):
+        s = 0
+        while i > 0:
+            s += self.tree[i]
+            i -= i & (-i)
+        return s
+
+    def range_query(self, l, r):
+        return self.query(r) - self.query(l - 1)
