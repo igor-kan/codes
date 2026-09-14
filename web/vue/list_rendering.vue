784d4902093dec846196f3c12b90d1381b47feba
igor-kan <igorkan010@gmail.com>
2026-09-14 18:40:37 -0400

    feat(web/vue): add list_rendering.vue

---
+++ web/vue/list_rendering.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+const users = ref([
+  { id: 1, name: "Ada" },
+  { id: 2, name: "Grace" },
+]);
+</script>
+
+<template>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+  </ul>
+  <p v-if="users.length === 0">No users.</p>
+  <p v-else>{{ users.length }} users.</p>
+</template>
