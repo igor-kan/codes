ffdad8f2eb0acbe6977a030bca84cd7111b19926
igor-kan <igorkan010@gmail.com>
2026-09-13 21:02:45 -0400

    feat(php/sorting): add bubble sort implementation

---
+++ algorithms/12_php/sorting/bubble_sort.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Algorithms\Sorting;
+
+function bubbleSort(array $arr): array {
+    $n = count($arr);
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $temp;
+            }
+        }
+    }
+    return $arr;
+}
