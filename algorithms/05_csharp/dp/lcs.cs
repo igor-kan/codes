5f19d41c77d8a8fddb0ea232f20517f5ec63877b
igor-kan <igorkan010@gmail.com>
2026-09-13 21:02:30 -0400

    feat(csharp/dp): add longest common subsequence algorithm

---
+++ algorithms/05_csharp/dp/lcs.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Algorithms.DynamicProgramming
+{
+    public static class LongestCommonSubsequence
+    {
+        public static int Compute(string a, string b)
+        {
+            int m = a.Length;
+            int n = b.Length;
+            int[,] dp = new int[m + 1, n + 1];
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (a[i - 1] == b[j - 1])
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                    else
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
