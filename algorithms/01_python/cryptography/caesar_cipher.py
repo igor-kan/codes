cfca889ce59f6213dbab09409cd74dd573942b2b
igor-kan <igorkan010@gmail.com>
2026-09-13 21:58:50 -0400

    feat(python/crypto): add caesar cipher

---
+++ algorithms/01_python/cryptography/caesar_cipher.py
@@ -0,0 +1,12 @@
+def encrypt(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            ascii_offset = 65 if char.isupper() else 97
+            result += chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+        else:
+            result += char
+    return result
+
+def decrypt(text, shift):
+    return encrypt(text, -shift)
