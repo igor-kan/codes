f22bacad943252c1a2a362365f238cbe83c7ce95
igor-kan <igorkan010@gmail.com>
2026-09-13 21:02:59 -0400

    feat(lua/sorting): add bubble sort implementation

---
+++ algorithms/19_lua/sorting/bubble_sort.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+function M.bubble_sort(arr)
+    local n = #arr
+    for i = 1, n - 1 do
+        for j = 1, n - i do
+            if arr[j] > arr[j + 1] then
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            end
+        end
+    end
+    return arr
+end
+
+return M
