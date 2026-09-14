4f3c0bb25ea716f362ff70ea9bee222725c0578d
igor-kan <igorkan010@gmail.com>
2026-09-14 18:40:42 -0400

    feat(web/vue): add v_model.vue

---
+++ web/vue/v_model.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { computed } from "vue";
+
+const model = defineModel({ type: String, default: "" });
+const length = computed(() => model.value.length);
+</script>
+
+<template>
+  <input v-model="model" placeholder="Type here" />
+  <small>{{ length }} characters</small>
+</template>
