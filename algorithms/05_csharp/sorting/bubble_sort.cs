87bec6360d36fa12bc8958c789a510d35d8e0e04
igor-kan <igorkan010@gmail.com>
2026-09-13 21:02:26 -0400

    feat(csharp/sorting): add bubble sort implementation

---
+++ algorithms/05_csharp/sorting/bubble_sort.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Algorithms.Sorting
+{
+    public static class BubbleSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+            }
+        }
+    }
+}
