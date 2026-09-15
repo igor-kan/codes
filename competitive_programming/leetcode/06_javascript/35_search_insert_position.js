05c6e43c0c0571714a5d239530cc5630acd83928
igor-kan <igorkan010@gmail.com>
2026-09-15 19:56:28 -0400

    feat(javascript/leetcode): add 35_search_insert_position.js

---
+++ competitive_programming/leetcode/06_javascript/35_search_insert_position.js
@@ -0,0 +1,16 @@
+function searchInsert(nums, target) {
+  let low = 0;
+  let high = nums.length;
+  while (low < high) {
+    const middle = (low + high) >> 1;
+    if (nums[middle] < target) low = middle + 1;
+    else high = middle;
+  }
+  return low;
+}
+
+if (require.main === module) {
+  if (searchInsert([1, 3, 5, 6], 5) !== 2 || searchInsert([1, 3, 5, 6], 2) !== 1) throw new Error("search insert failed");
+  if (searchInsert([1, 3, 5, 6], 7) !== 4 || searchInsert([1, 3, 5, 6], 0) !== 0) throw new Error("search insert failed");
+  console.log("35 search insert position ok");
+}
