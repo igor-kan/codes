4c2142fab8998bfaa27e3e3e2bdc8eab5c05147c
igor-kan <igorkan010@gmail.com>
2026-09-15 19:53:08 -0400

    feat(javascript/codeforces): add 282a_bit_plus_plus.js

---
+++ competitive_programming/codeforces/06_javascript/282a_bit_plus_plus.js
@@ -0,0 +1,11 @@
+function bitPlusPlus(operations) {
+  let value = 0;
+  for (const operation of operations) value += operation.includes("++") ? 1 : -1;
+  return value;
+}
+
+if (require.main === module) {
+  if (bitPlusPlus(["++X", "X++", "--X"]) !== 1) throw new Error("bit++ failed");
+  if (bitPlusPlus(["X++", "X++", "X++", "X--"]) !== 2) throw new Error("bit++ failed");
+  console.log("282A bit++ ok");
+}
