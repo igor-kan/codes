bd18aa931e00e686aa3541e0653b5ffc9c263c64
igor-kan <igorkan010@gmail.com>
2026-09-15 19:56:33 -0400

    feat(javascript/leetcode): add 69_sqrt_x.js

---
+++ competitive_programming/leetcode/06_javascript/69_sqrt_x.js
@@ -0,0 +1,15 @@
+function sqrtX(x) {
+  let low = 0;
+  let high = x;
+  while (low <= high) {
+    const middle = Math.floor((low + high) / 2);
+    if (middle * middle <= x) low = middle + 1;
+    else high = middle - 1;
+  }
+  return high;
+}
+
+if (require.main === module) {
+  if (sqrtX(4) !== 2 || sqrtX(8) !== 2 || sqrtX(0) !== 0) throw new Error("sqrt x failed");
+  console.log("69 sqrt x ok");
+}
