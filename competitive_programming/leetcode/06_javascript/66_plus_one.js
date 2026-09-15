97170c53de9099bbae5e22aa0d36a5992233290a
igor-kan <igorkan010@gmail.com>
2026-09-15 19:56:31 -0400

    feat(javascript/leetcode): add 66_plus_one.js

---
+++ competitive_programming/leetcode/06_javascript/66_plus_one.js
@@ -0,0 +1,14 @@
+function plusOne(digits) {
+  const result = [...digits];
+  for (let i = result.length - 1; i >= 0; i -= 1) {
+    if (result[i] < 9) { result[i] += 1; return result; }
+    result[i] = 0;
+  }
+  return [1, ...result];
+}
+
+if (require.main === module) {
+  if (plusOne([1, 2, 3]).join(",") !== "1,2,4") throw new Error("plus one failed");
+  if (plusOne([9, 9]).join(",") !== "1,0,0") throw new Error("plus one failed");
+  console.log("66 plus one ok");
+}
