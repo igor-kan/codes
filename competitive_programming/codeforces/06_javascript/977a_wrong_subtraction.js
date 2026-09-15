92f6937e199ccf255474efcdc2fd9155c08d9bba
igor-kan <igorkan010@gmail.com>
2026-09-15 19:53:17 -0400

    feat(javascript/codeforces): add 977a_wrong_subtraction.js

---
+++ competitive_programming/codeforces/06_javascript/977a_wrong_subtraction.js
@@ -0,0 +1,14 @@
+function wrongSubtraction(number, steps) {
+  let current = number;
+  for (let i = 0; i < steps; i += 1) {
+    if (current % 10 === 0) current /= 10;
+    else current -= 1;
+  }
+  return current;
+}
+
+if (require.main === module) {
+  if (wrongSubtraction(512, 4) !== 50) throw new Error("wrong subtraction failed");
+  if (wrongSubtraction(1000000000, 9) !== 1) throw new Error("wrong subtraction failed");
+  console.log("977A wrong subtraction ok");
+}
