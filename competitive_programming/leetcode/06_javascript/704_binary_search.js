10d20dd18fb3f3ca2f463b8a4cc42f7fcb1df052
igor-kan <igorkan010@gmail.com>
2026-09-15 19:56:34 -0400

    feat(javascript/leetcode): add 704_binary_search.js

---
+++ competitive_programming/leetcode/06_javascript/704_binary_search.js
@@ -0,0 +1,16 @@
+function binarySearch(nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+  while (low <= high) {
+    const middle = (low + high) >> 1;
+    if (nums[middle] === target) return middle;
+    if (nums[middle] < target) low = middle + 1;
+    else high = middle - 1;
+  }
+  return -1;
+}
+
+if (require.main === module) {
+  if (binarySearch([-1, 0, 3, 5, 9, 12], 9) !== 4 || binarySearch([-1, 0, 3, 5, 9, 12], 2) !== -1) throw new Error("binary search failed");
+  console.log("704 binary search ok");
+}
