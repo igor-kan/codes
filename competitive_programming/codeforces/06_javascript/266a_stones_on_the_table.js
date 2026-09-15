26d17ab5eb0b8d5e04700c660944fc3b2600e5cd
igor-kan <igorkan010@gmail.com>
2026-09-15 19:53:06 -0400

    feat(javascript/codeforces): add 266a_stones_on_the_table.js

---
+++ competitive_programming/codeforces/06_javascript/266a_stones_on_the_table.js
@@ -0,0 +1,12 @@
+function stonesOnTable(row) {
+  let removals = 0;
+  for (let i = 1; i < row.length; i += 1) if (row[i] === row[i - 1]) removals += 1;
+  return removals;
+}
+
+if (require.main === module) {
+  if (stonesOnTable("RRG") !== 1 || stonesOnTable("RRRRR") !== 4 || stonesOnTable("BRBG") !== 0) {
+    throw new Error("stones on the table failed");
+  }
+  console.log("266A stones on the table ok");
+}
