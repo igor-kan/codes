74a1895a1e07751e97ca36fd2d22eeeb1bd7931f
igor-kan <igorkan010@gmail.com>
2026-09-15 19:59:49 -0400

    feat(typescript/leetcode): add 268_missing_number.ts

---
+++ competitive_programming/leetcode/07_typescript/268_missing_number.ts
@@ -0,0 +1,7 @@
+function missingNumber(nums: number[]): number {
+  const n = nums.length;
+  return (n * (n + 1)) / 2 - nums.reduce((a, b) => a + b, 0);
+}
+
+if (missingNumber([3, 0, 1]) !== 2 || missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]) !== 8) throw new Error("missing number failed");
+console.log("268 missing number ok");
