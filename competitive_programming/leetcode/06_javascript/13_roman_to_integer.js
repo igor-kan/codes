5874ad54336074a6cff6360ea420db457179551a
igor-kan <igorkan010@gmail.com>
2026-09-15 19:56:13 -0400

    feat(javascript/leetcode): add 13_roman_to_integer.js

---
+++ competitive_programming/leetcode/06_javascript/13_roman_to_integer.js
@@ -0,0 +1,16 @@
+const VALUES = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+function romanToInteger(text) {
+  let total = 0;
+  for (let i = 0; i < text.length; i += 1) {
+    const value = VALUES[text[i]];
+    if (i + 1 < text.length && value < VALUES[text[i + 1]]) total -= value;
+    else total += value;
+  }
+  return total;
+}
+
+if (require.main === module) {
+  if (romanToInteger("III") !== 3 || romanToInteger("LVIII") !== 58 || romanToInteger("MCMXCIV") !== 1994) throw new Error("roman to integer failed");
+  console.log("13 roman to integer ok");
+}
