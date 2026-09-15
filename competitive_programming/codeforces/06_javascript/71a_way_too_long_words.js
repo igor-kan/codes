45d4da7c386760c1a49e2ac763af000f4989808e
igor-kan <igorkan010@gmail.com>
2026-09-15 19:53:15 -0400

    feat(javascript/codeforces): add 71a_way_too_long_words.js

---
+++ competitive_programming/codeforces/06_javascript/71a_way_too_long_words.js
@@ -0,0 +1,10 @@
+function wayTooLong(word) {
+  return word.length <= 10 ? word : `${word[0]}${word.length - 2}${word[word.length - 1]}`;
+}
+
+if (require.main === module) {
+  if (wayTooLong("word") !== "word") throw new Error("way too long failed");
+  if (wayTooLong("localization") !== "l10n") throw new Error("way too long failed");
+  if (wayTooLong("internationalization") !== "i18n") throw new Error("way too long failed");
+  console.log("71A way too long words ok");
+}
