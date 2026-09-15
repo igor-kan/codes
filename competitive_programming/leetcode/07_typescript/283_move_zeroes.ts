44adc198ca9af136b0818dbb6505e5519f38990b
igor-kan <igorkan010@gmail.com>
2026-09-15 19:59:52 -0400

    feat(typescript/leetcode): add 283_move_zeroes.ts

---
+++ competitive_programming/leetcode/07_typescript/283_move_zeroes.ts
@@ -0,0 +1,9 @@
+function moveZeroes(nums: number[]): number[] {
+  const result = nums.filter((number) => number !== 0);
+  while (result.length < nums.length) result.push(0);
+  return result;
+}
+
+if (moveZeroes([0, 1, 0, 3, 12]).join(",") !== "1,3,12,0,0") throw new Error("move zeroes failed");
+if (moveZeroes([0]).join(",") !== "0") throw new Error("move zeroes failed");
+console.log("283 move zeroes ok");
