d79cbc282ae7dca39135890879f6198999202d2c
igor-kan <igorkan010@gmail.com>
2026-09-15 19:56:24 -0400

    feat(javascript/leetcode): add 27_remove_element.js

---
+++ competitive_programming/leetcode/06_javascript/27_remove_element.js
@@ -0,0 +1,9 @@
+function removeElement(nums, value) {
+  return nums.filter((number) => number !== value);
+}
+
+if (require.main === module) {
+  if (removeElement([3, 2, 2, 3], 3).join(",") !== "2,2") throw new Error("remove element failed");
+  if (removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2).join(",") !== "0,1,3,0,4") throw new Error("remove element failed");
+  console.log("27 remove element ok");
+}
