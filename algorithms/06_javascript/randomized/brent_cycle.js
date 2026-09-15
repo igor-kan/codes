3a9d24bbb9b0834eb63adfe342e8280bd89bfe8c
igor-kan <igorkan010@gmail.com>
2026-09-15 19:42:58 -0400

    feat(javascript/randomized): add brent_cycle.js

---
+++ algorithms/06_javascript/randomized/brent_cycle.js
@@ -0,0 +1,26 @@
+function brentCycle(nextNode, start) {
+  let power = 1;
+  let lam = 1;
+  let tortoise = start;
+  let hare = nextNode(start);
+  while (tortoise !== hare) {
+    if (power === lam) { tortoise = hare; power *= 2; lam = 0; }
+    hare = nextNode(hare);
+    lam += 1;
+  }
+  tortoise = start;
+  hare = start;
+  for (let i = 0; i < lam; i += 1) hare = nextNode(hare);
+  let mu = 0;
+  while (tortoise !== hare) { tortoise = nextNode(tortoise); hare = nextNode(hare); mu += 1; }
+  return { mu, lambda: lam };
+}
+
+module.exports = { brentCycle };
+
+if (require.main === module) {
+  const link = [1, 2, 3, 4, 3];
+  const { mu, lambda } = brentCycle((i) => link[i], 0);
+  if (mu !== 3 || lambda !== 2) throw new Error("brent cycle failed");
+  console.log("[JavaScript Brent Cycle] cycle entry and length verified");
+}
