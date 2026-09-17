ba66734c95cfa85fafa5bf32b3fc0af15651c57d
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:15 -0400

    feat(searching): add randomized heap algorithm implementation

---
+++ algorithms/01_python/searching/randomized_heap.py
@@ -0,0 +1,14 @@
+"""
+Implementation of randomized heap algorithm.
+"""
+
+def randomized_heap_run(data):
+    """
+    Executes the randomized heap logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running randomized_heap:", randomized_heap_run(sample_data))
