58f7b0f7e44794af973b2d123adda26b89e2e8d0
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:35 -0400

    feat(greedy): add bidirectional path algorithm implementation

---
+++ algorithms/01_python/greedy/bidirectional_path.py
@@ -0,0 +1,14 @@
+"""
+Implementation of bidirectional path algorithm.
+"""
+
+def bidirectional_path_run(data):
+    """
+    Executes the bidirectional path logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running bidirectional_path:", bidirectional_path_run(sample_data))
