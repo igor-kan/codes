6a5cc05eecd105cb81503663fe495bb841c30c34
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:23 -0400

    feat(greedy): add bidirectional queue algorithm implementation

---
+++ algorithms/01_python/greedy/bidirectional_queue.py
@@ -0,0 +1,14 @@
+"""
+Implementation of bidirectional queue algorithm.
+"""
+
+def bidirectional_queue_run(data):
+    """
+    Executes the bidirectional queue logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running bidirectional_queue:", bidirectional_queue_run(sample_data))
