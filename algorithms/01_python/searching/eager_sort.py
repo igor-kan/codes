61016b2529fde5dba8887430e51bf1b5d2df0b66
igor-kan <igorkan010@gmail.com>
2026-09-16 23:39:45 -0400

    feat(searching): add eager sort algorithm implementation

---
+++ algorithms/01_python/searching/eager_sort.py
@@ -0,0 +1,14 @@
+"""
+Implementation of eager sort algorithm.
+"""
+
+def eager_sort_run(data):
+    """
+    Executes the eager sort logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running eager_sort:", eager_sort_run(sample_data))
