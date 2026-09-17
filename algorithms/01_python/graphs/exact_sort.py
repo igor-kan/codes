2fccd3c94e4fab387cf6f8ea5776d1bcf2f93881
igor-kan <igorkan010@gmail.com>
2026-09-16 23:37:58 -0400

    feat(graphs): add exact sort algorithm implementation

---
+++ algorithms/01_python/graphs/exact_sort.py
@@ -0,0 +1,14 @@
+"""
+Implementation of exact sort algorithm.
+"""
+
+def exact_sort_run(data):
+    """
+    Executes the exact sort logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running exact_sort:", exact_sort_run(sample_data))
