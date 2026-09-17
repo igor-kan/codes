c410aa939b9cbdaa7424c1df4df450d189ebb00c
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:33 -0400

    feat(geometry): add bidirectional sort algorithm implementation

---
+++ algorithms/01_python/geometry/bidirectional_sort.py
@@ -0,0 +1,14 @@
+"""
+Implementation of bidirectional sort algorithm.
+"""
+
+def bidirectional_sort_run(data):
+    """
+    Executes the bidirectional sort logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running bidirectional_sort:", bidirectional_sort_run(sample_data))
