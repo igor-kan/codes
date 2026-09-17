c0d6b5c9c65a9960094a06ce1747cf01689ee23d
igor-kan <igorkan010@gmail.com>
2026-09-16 23:39:21 -0400

    feat(cryptography): add in_place sort algorithm implementation

---
+++ algorithms/01_python/cryptography/in_place_sort.py
@@ -0,0 +1,14 @@
+"""
+Implementation of in_place sort algorithm.
+"""
+
+def in_place_sort_run(data):
+    """
+    Executes the in_place sort logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running in_place_sort:", in_place_sort_run(sample_data))
