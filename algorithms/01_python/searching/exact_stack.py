456be2558454fb69f1df1ab5f162ac0bb520a5fd
igor-kan <igorkan010@gmail.com>
2026-09-16 23:39:19 -0400

    feat(searching): add exact stack algorithm implementation

---
+++ algorithms/01_python/searching/exact_stack.py
@@ -0,0 +1,14 @@
+"""
+Implementation of exact stack algorithm.
+"""
+
+def exact_stack_run(data):
+    """
+    Executes the exact stack logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running exact_stack:", exact_stack_run(sample_data))
