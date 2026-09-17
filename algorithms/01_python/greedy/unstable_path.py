bf894e1fbfe97bbe03765c3c9f4e0a575ccd7f89
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:22 -0400

    feat(greedy): add unstable path algorithm implementation

---
+++ algorithms/01_python/greedy/unstable_path.py
@@ -0,0 +1,14 @@
+"""
+Implementation of unstable path algorithm.
+"""
+
+def unstable_path_run(data):
+    """
+    Executes the unstable path logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running unstable_path:", unstable_path_run(sample_data))
