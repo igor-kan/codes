626bdc935af73e5f5eb090a3f41f2e488530b859
igor-kan <igorkan010@gmail.com>
2026-09-16 23:37:32 -0400

    feat(graphs): add in_place stack algorithm implementation

---
+++ algorithms/01_python/graphs/in_place_stack.py
@@ -0,0 +1,14 @@
+"""
+Implementation of in_place stack algorithm.
+"""
+
+def in_place_stack_run(data):
+    """
+    Executes the in_place stack logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running in_place_stack:", in_place_stack_run(sample_data))
