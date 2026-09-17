4882cfd796af8d9799b9117e175ed9fd9c16409f
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:26 -0400

    feat(strings): add in_place queue algorithm implementation

---
+++ algorithms/01_python/strings/in_place_queue.py
@@ -0,0 +1,14 @@
+"""
+Implementation of in_place queue algorithm.
+"""
+
+def in_place_queue_run(data):
+    """
+    Executes the in_place queue logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running in_place_queue:", in_place_queue_run(sample_data))
