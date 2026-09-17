718f226c8f862db0b62b8cad8f7ab1f766c00bca
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:04 -0400

    feat(strings): add in_place intersect algorithm implementation

---
+++ algorithms/01_python/strings/in_place_intersect.py
@@ -0,0 +1,14 @@
+"""
+Implementation of in_place intersect algorithm.
+"""
+
+def in_place_intersect_run(data):
+    """
+    Executes the in_place intersect logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running in_place_intersect:", in_place_intersect_run(sample_data))
