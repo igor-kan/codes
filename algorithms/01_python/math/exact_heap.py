72f289b533d129730c988bf7160fc1af2b49852a
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:21 -0400

    feat(math): add exact heap algorithm implementation

---
+++ algorithms/01_python/math/exact_heap.py
@@ -0,0 +1,14 @@
+"""
+Implementation of exact heap algorithm.
+"""
+
+def exact_heap_run(data):
+    """
+    Executes the exact heap logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running exact_heap:", exact_heap_run(sample_data))
