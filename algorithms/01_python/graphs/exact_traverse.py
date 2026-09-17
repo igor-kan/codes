45b232540c58395b3f0cf909794aa827430096d4
igor-kan <igorkan010@gmail.com>
2026-09-16 23:37:39 -0400

    feat(graphs): add exact traverse algorithm implementation

---
+++ algorithms/01_python/graphs/exact_traverse.py
@@ -0,0 +1,14 @@
+"""
+Implementation of exact traverse algorithm.
+"""
+
+def exact_traverse_run(data):
+    """
+    Executes the exact traverse logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running exact_traverse:", exact_traverse_run(sample_data))
