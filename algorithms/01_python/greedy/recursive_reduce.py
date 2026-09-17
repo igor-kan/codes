ae5608c2216242720eff4fef9d7284dcae8e92cd
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:43 -0400

    feat(greedy): add recursive reduce algorithm implementation

---
+++ algorithms/01_python/greedy/recursive_reduce.py
@@ -0,0 +1,14 @@
+"""
+Implementation of recursive reduce algorithm.
+"""
+
+def recursive_reduce_run(data):
+    """
+    Executes the recursive reduce logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running recursive_reduce:", recursive_reduce_run(sample_data))
