26b5395e8d6b4f62cc57c1daea5c7d98a5322a36
igor-kan <igorkan010@gmail.com>
2026-09-16 23:39:31 -0400

    feat(graphs): add exact reduce algorithm implementation

---
+++ algorithms/01_python/graphs/exact_reduce.py
@@ -0,0 +1,14 @@
+"""
+Implementation of exact reduce algorithm.
+"""
+
+def exact_reduce_run(data):
+    """
+    Executes the exact reduce logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running exact_reduce:", exact_reduce_run(sample_data))
