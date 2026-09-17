5cb5ffca3c9fb07325d81ce7609e2ddb792959d9
igor-kan <igorkan010@gmail.com>
2026-09-16 23:38:16 -0400

    feat(graphs): add iterative heap algorithm implementation

---
+++ algorithms/01_python/graphs/iterative_heap.py
@@ -0,0 +1,14 @@
+"""
+Implementation of iterative heap algorithm.
+"""
+
+def iterative_heap_run(data):
+    """
+    Executes the iterative heap logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running iterative_heap:", iterative_heap_run(sample_data))
