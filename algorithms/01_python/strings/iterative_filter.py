d5bf6d8744e86a076c3a9b449fe1d239d2270cba
igor-kan <igorkan010@gmail.com>
2026-09-16 23:39:28 -0400

    feat(strings): add iterative filter algorithm implementation

---
+++ algorithms/01_python/strings/iterative_filter.py
@@ -0,0 +1,14 @@
+"""
+Implementation of iterative filter algorithm.
+"""
+
+def iterative_filter_run(data):
+    """
+    Executes the iterative filter logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running iterative_filter:", iterative_filter_run(sample_data))
