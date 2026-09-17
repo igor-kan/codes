af7bbe74fc86a117aecc078d5dc2775c24983fce
igor-kan <igorkan010@gmail.com>
2026-09-16 23:37:51 -0400

    feat(greedy): add iterative search algorithm implementation

---
+++ algorithms/01_python/greedy/iterative_search.py
@@ -0,0 +1,14 @@
+"""
+Implementation of iterative search algorithm.
+"""
+
+def iterative_search_run(data):
+    """
+    Executes the iterative search logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running iterative_search:", iterative_search_run(sample_data))
