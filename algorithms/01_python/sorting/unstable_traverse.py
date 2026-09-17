a4bbe65beeccdca2509b93b3f6ff6a230c3aa77b
igor-kan <igorkan010@gmail.com>
2026-09-16 23:37:54 -0400

    feat(sorting): add unstable traverse algorithm implementation

---
+++ algorithms/01_python/sorting/unstable_traverse.py
@@ -0,0 +1,14 @@
+"""
+Implementation of unstable traverse algorithm.
+"""
+
+def unstable_traverse_run(data):
+    """
+    Executes the unstable traverse logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running unstable_traverse:", unstable_traverse_run(sample_data))
