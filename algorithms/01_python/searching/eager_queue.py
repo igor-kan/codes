6d2e2ef57fd64007ee8a3bb70970d70ee85e521f
igor-kan <igorkan010@gmail.com>
2026-09-16 23:39:16 -0400

    feat(searching): add eager queue algorithm implementation

---
+++ algorithms/01_python/searching/eager_queue.py
@@ -0,0 +1,14 @@
+"""
+Implementation of eager queue algorithm.
+"""
+
+def eager_queue_run(data):
+    """
+    Executes the eager queue logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running eager_queue:", eager_queue_run(sample_data))
