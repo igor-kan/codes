02abbf57aad4e0505e7d694a8360bc61e8845267
igor-kan <igorkan010@gmail.com>
2026-09-16 23:39:09 -0400

    feat(math): add in_place search algorithm implementation

---
+++ algorithms/01_python/math/in_place_search.py
@@ -0,0 +1,14 @@
+"""
+Implementation of in_place search algorithm.
+"""
+
+def in_place_search_run(data):
+    """
+    Executes the in_place search logic.
+    """
+    # TODO: Complete implementation
+    return data
+
+if __name__ == "__main__":
+    sample_data = [1, 2, 3, 4, 5]
+    print("Running in_place_search:", in_place_search_run(sample_data))
