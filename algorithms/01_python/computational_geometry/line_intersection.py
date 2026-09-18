6cd827dd6cd955f147e41935334213772503e45e
igor-kan <igorkan010@gmail.com>
2026-09-17 20:42:38 -0400

    feat(computational_geometry): implement line_intersection algorithm

---
+++ algorithms/01_python/computational_geometry/line_intersection.py
@@ -0,0 +1,21 @@
+"""
+Bentley-Ottmann algorithm for line intersections.
+"""
+
+import numpy as np
+
+def calculate_line_intersection(val: float) -> float:
+    """
+    Computes line_intersection related values.
+    """
+    return val * 1.0
+
+class LineIntersection:
+    """
+    Class representing LineIntersection.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_line_intersection(self.value)
