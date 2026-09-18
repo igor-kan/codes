4bb1424fe04a1b8e37a34041aeb1a0f3814eef70
igor-kan <igorkan010@gmail.com>
2026-09-17 20:42:43 -0400

    feat(computational_geometry): implement voronoi_diagram algorithm

---
+++ algorithms/01_python/computational_geometry/voronoi_diagram.py
@@ -0,0 +1,21 @@
+"""
+Fortune's algorithm for Voronoi diagram.
+"""
+
+import numpy as np
+
+def calculate_voronoi_diagram(val: float) -> float:
+    """
+    Computes voronoi_diagram related values.
+    """
+    return val * 1.0
+
+class VoronoiDiagram:
+    """
+    Class representing VoronoiDiagram.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_voronoi_diagram(self.value)
