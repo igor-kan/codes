a5597a40e1d62a8e64a127cb6a473d339a21589a
igor-kan <igorkan010@gmail.com>
2026-09-17 20:42:39 -0400

    feat(computational_geometry): implement graham_scan algorithm

---
+++ algorithms/01_python/computational_geometry/graham_scan.py
@@ -0,0 +1,21 @@
+"""
+Convex hull using Graham scan.
+"""
+
+import numpy as np
+
+def calculate_graham_scan(val: float) -> float:
+    """
+    Computes graham_scan related values.
+    """
+    return val * 1.0
+
+class GrahamScan:
+    """
+    Class representing GrahamScan.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_graham_scan(self.value)
