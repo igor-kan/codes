c832086611323ca0541fc5da33d564bf184d8ea9
igor-kan <igorkan010@gmail.com>
2026-09-17 20:42:42 -0400

    feat(computational_geometry): implement closest_pair algorithm

---
+++ algorithms/01_python/computational_geometry/closest_pair.py
@@ -0,0 +1,21 @@
+"""
+Divide and conquer closest pair of points.
+"""
+
+import numpy as np
+
+def calculate_closest_pair(val: float) -> float:
+    """
+    Computes closest_pair related values.
+    """
+    return val * 1.0
+
+class ClosestPair:
+    """
+    Class representing ClosestPair.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_closest_pair(self.value)
