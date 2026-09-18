5c35c519fe45c66730a84a6c65c46c93e5243318
igor-kan <igorkan010@gmail.com>
2026-09-17 20:43:45 -0400

    feat(signal_processing): implement discrete_fourier_transform algorithm

---
+++ algorithms/01_python/signal_processing/discrete_fourier_transform.py
@@ -0,0 +1,21 @@
+"""
+O(N^2) Discrete Fourier Transform.
+"""
+
+import numpy as np
+
+def calculate_discrete_fourier_transform(val: float) -> float:
+    """
+    Computes discrete_fourier_transform related values.
+    """
+    return val * 1.0
+
+class DiscreteFourierTransform:
+    """
+    Class representing DiscreteFourierTransform.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_discrete_fourier_transform(self.value)
