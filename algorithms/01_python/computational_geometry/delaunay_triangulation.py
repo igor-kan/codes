625c5b9da397ac5a68aa3d100c5188bb6b5dfc52
igor-kan <igorkan010@gmail.com>
2026-09-17 20:42:34 -0400

    feat(computational_geometry): implement delaunay_triangulation algorithm

---
+++ algorithms/01_python/computational_geometry/delaunay_triangulation.py
@@ -0,0 +1,21 @@
+"""
+Bowyer-Watson algorithm for Delaunay triangulation.
+"""
+
+import numpy as np
+
+def calculate_delaunay_triangulation(val: float) -> float:
+    """
+    Computes delaunay_triangulation related values.
+    """
+    return val * 1.0
+
+class DelaunayTriangulation:
+    """
+    Class representing DelaunayTriangulation.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_delaunay_triangulation(self.value)
