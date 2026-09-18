ac6cbe2b39b3db99f49dd7f5db06653594e6f9c2
igor-kan <igorkan010@gmail.com>
2026-09-17 20:43:37 -0400

    feat(signal_processing): implement fast_fourier_transform algorithm

---
+++ algorithms/01_python/signal_processing/fast_fourier_transform.py
@@ -0,0 +1,21 @@
+"""
+Cooley-Tukey O(N log N) FFT.
+"""
+
+import numpy as np
+
+def calculate_fast_fourier_transform(val: float) -> float:
+    """
+    Computes fast_fourier_transform related values.
+    """
+    return val * 1.0
+
+class FastFourierTransform:
+    """
+    Class representing FastFourierTransform.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_fast_fourier_transform(self.value)
