cc110a0fce8d8f627d92b8459236bdf4288f5913
igor-kan <igorkan010@gmail.com>
2026-09-17 20:43:23 -0400

    feat(information_theory): implement huffman_coding algorithm

---
+++ algorithms/01_python/information_theory/huffman_coding.py
@@ -0,0 +1,21 @@
+"""
+Huffman coding tree generation.
+"""
+
+import numpy as np
+
+def calculate_huffman_coding(val: float) -> float:
+    """
+    Computes huffman_coding related values.
+    """
+    return val * 1.0
+
+class HuffmanCoding:
+    """
+    Class representing HuffmanCoding.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_huffman_coding(self.value)
