ec63f52821395af2f995a8834b2702538452a7e3
igor-kan <igorkan010@gmail.com>
2026-09-17 20:43:22 -0400

    feat(information_theory): implement arithmetic_coding algorithm

---
+++ algorithms/01_python/information_theory/arithmetic_coding.py
@@ -0,0 +1,21 @@
+"""
+Arithmetic coding for data compression.
+"""
+
+import numpy as np
+
+def calculate_arithmetic_coding(val: float) -> float:
+    """
+    Computes arithmetic_coding related values.
+    """
+    return val * 1.0
+
+class ArithmeticCoding:
+    """
+    Class representing ArithmeticCoding.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_arithmetic_coding(self.value)
