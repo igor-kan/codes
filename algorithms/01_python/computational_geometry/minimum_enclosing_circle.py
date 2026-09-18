62011fcf02435511d6267099d8b8d2902850052f
igor-kan <igorkan010@gmail.com>
2026-09-17 20:42:36 -0400

    feat(computational_geometry): implement minimum_enclosing_circle algorithm

---
+++ algorithms/01_python/computational_geometry/minimum_enclosing_circle.py
@@ -0,0 +1,21 @@
+"""
+Welzl's algorithm for minimum enclosing circle.
+"""
+
+import numpy as np
+
+def calculate_minimum_enclosing_circle(val: float) -> float:
+    """
+    Computes minimum_enclosing_circle related values.
+    """
+    return val * 1.0
+
+class MinimumEnclosingCircle:
+    """
+    Class representing MinimumEnclosingCircle.
+    """
+    def __init__(self, value: float):
+        self.value = value
+
+    def compute(self) -> float:
+        return calculate_minimum_enclosing_circle(self.value)
