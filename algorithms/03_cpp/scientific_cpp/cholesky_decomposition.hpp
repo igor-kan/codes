283cccb0fcd9b6c59865b051cd31cc24c7c4d692
igor-kan <igorkan010@gmail.com>
2026-09-21 10:15:03 -0400

    feat(scientific_cpp): add header definition for cholesky decomposition

---
+++ algorithms/03_cpp/scientific_cpp/cholesky_decomposition.hpp
@@ -0,0 +1,8 @@
+#ifndef CHOLESKY_DECOMPOSITION_HPP
+#define CHOLESKY_DECOMPOSITION_HPP
+
+#include <vector>
+
+bool cholesky_factorize(const std::vector<std::vector<double>>& A, std::vector<std::vector<double>>& L);
+
+#endif
